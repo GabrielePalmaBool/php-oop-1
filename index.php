c09536f1e472cf1ca0a36663ba6ba09d7c09895a
Gabriele <gabriele.palma.bool@gmail.com>
2024-02-02 15:50:59 +0100

    add sub-class

--- index.php
+++ index.php
@@ -16,10 +16,10 @@ class Movie {
     public $data_uscita;
     public $casa_produzione;
     public $durata;
-    public $genere;
+    public  Genere $genere;
 
     //dichiaro il costruttore
-    public function __construct($titolo,$data_uscita,$casa_produzione,$durata,$genere) {
+    public function __construct($titolo,$data_uscita,$casa_produzione,$durata,Genere $genere) {
 
         $this->titolo = $titolo;
         $this->data_uscita = $data_uscita;
@@ -34,6 +34,9 @@ class Movie {
         $this->titolo = $titolo;
     }
 
+    public function setDurata($durata) {
+        $this->durata = $durata;
+    }
     //funzioni get
     public function getTitolo() {
         return $this->titolo;
@@ -43,15 +46,50 @@ class Movie {
         return $this->data_uscita;
     }
 
+    public function getDurata() {
+        return $this->durata;
+    }
+
+}
+
+class Genere {
+    public $uno;
+    public $due;
+
+    public $tre;
+
+    //dichiaro il costruttore
+    public function __construct($uno,$due,$tre) {
+
+        $this->uno = $uno;
+        $this->due = $due;
+        $this->tre = $tre;
+
+    }
+
 }
 
 
-//istanzio il prio oggetto movie con questi valori
-$movie1 = new Movie('primo','2020-05-03','rossa','218 min','guerra');
+$gen1 = new Genere('guerra','azione','drammatico');
 
+$gen2 = new Genere('ridicolo','avventura','romantico');
+
+//istanzio il prio oggetto movie con questi valori
+$movie1 = new Movie('primo','2020-05-03','rossa','218 min',$gen1);
+
+//istanzio il prio oggetto movie con questi valori
+$movie2 = new Movie('secodno','1978-07-14','blu','145 min',$gen2);
+
+
+
+var_dump($movie1-> genere ->due);
 
 
 //eseguo varie prove
+$movie2->setDurata('32 min');
+
+var_dump($movie2->getDurata());
+
 var_dump($movie1->getTitolo());
 
 $movie1->setTitolo('poldo');
@@ -60,6 +98,7 @@ var_dump($movie1->getTitolo());
 
 
 ?>
-    
+
+
 </body>
 </html>
